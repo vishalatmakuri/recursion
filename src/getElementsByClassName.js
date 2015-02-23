34d9bf74e59c97f7a3475afc809ae146d72e9f6b
vishalatmakuri <findvishal@gmail.com>
2015-02-22 21:25:51 -0600

    Solved Basic Requirements Recursion
    Basic Requirements
    stringifyJSON: Complete
    getElementsByClassName: Complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,4 +1,4 @@
-// If life was easy, we could just do things the easy way:
+// If life was easy, we could just do things the  easy way:
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
@@ -6,5 +6,36 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
+	var $body = document.body;
+	var result=[];
+	var lookForclass = function (node) {
+			if(node.classList=== undefined){}
+			else{
+				console.log(node.classList);
+				count = node.classList.length-1;
+				while (count>=0){
+				if( node.classList[count]===className){
+					result.push(node);
+						break;
+					}
+					count--;
+				}
+			}
+	};
+	var htmlDigger = function (node) {
+		var nodeLen = node.childNodes.length-1;
+		lookForclass(node)
+		if(nodeLen>=0){
+				
+			while(nodeLen>=0){
+			htmlDigger(node.childNodes[nodeLen]);
+			nodeLen--;
+			}
+		}
+	};
+	htmlDigger($body);
+
+	return result;
   // your code here
 };
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,88 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+var arr =[];
+var result;
+
+var test_basic = function (val){
+	var temp;
+	if(val === null){arr.push('null');}
+
+	else if (typeof(val)==='string'){
+		arr.push('"'+val+'"');
+	} 
+
+	else if(Array.isArray(val)){
+		arr.push("]");
+
+		if(val.length>1){
+			while (val.length>1){
+				temp = val[val.length-1];
+				var dummy =val.pop();
+				test_basic(temp);
+				arr.push(',');
+			}
+			test_basic(val[0]);
+		}
+		else if(val[0] === undefined){}
+		else{
+			test_basic(val[0]);
+			}
+		arr.push("[");
+	}
+	else if(val !== null && typeof val === 'object'){
+			arr.push("}");
+			var list=[];
+			var count=0;
+			for(var x in val){ list.push(x);
+			}
+			count= list.length-1;
+			if (count>=1){
+				while(count>=0){
+					if(typeof(val[list[count]])==="function"||typeof(val[list[count]])==="undefined"){}
+
+					else if(count===(list.length-1)){
+						test_basic(val[list[count]]);
+						arr.push(":");
+						test_basic(list[count]);
+					}
+					else{
+						arr.push(",");
+						test_basic(val[list[count]]);
+						arr.push(":");
+						test_basic(list[count]);	
+					}
+					count--;
+				}
+			}
+		    else {
+				if(val[list[0]]=== undefined){}
+				else{	
+					test_basic(val[list[count]]);
+					arr.push(":");
+					test_basic(list[count]);
+				}
+			}
+			arr.push("{");
+	}
+	else{ 
+ 		var type = val.toString();
+ 		arr.push(type);}
+
+};
+var strOut= function (x) {
+	var temp;
+	var	len =x.length-1;
+	if (len === 0){
+		return x[0];
+	}
+	else{
+		temp = x.pop();
+		return temp+strOut(x); 
+	}
+};
+	test_basic(obj);
+	result = strOut(arr);
+
+	return result;
 };
